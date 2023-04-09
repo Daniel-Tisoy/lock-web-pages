7982ff0b51f3a22b2b66ab1013aa1b1eb4c61d88
dtisoy <danieltisoy2001@gmail.com>
2023-04-09 13:29:03 -0500

    fix: catch the error when creating the table
    the error is catching directly in the DbQueries class

--- db_config.py
+++ db_config.py
@@ -1,7 +1,6 @@
 from db import DbQueries
 from os import getenv
 from dotenv import load_dotenv
-from sqlite3 import OperationalError
 
 load_dotenv()
 # load db configuration
@@ -12,8 +11,5 @@ db_id_column = getenv("DB_ID_COLUMN_NAME")
 
 if __name__ == "__main__":
     db_manager = DbQueries(db_name, db_table, db_column, db_id_column)
-    try:
-        db_manager.create_table()
-        print("db table created succesfully")
-    except OperationalError:
-        print(f"db table {db_table} already exists")
+    result = db_manager.create_table()
+    print(result)
